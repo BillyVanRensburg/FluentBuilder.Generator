2f57b64f3fb45688fdca70852a3fe5eec6a0d6e0
BillyVanRensburg <billy.van.rensburg@outlook.com>
2026-03-18 13:06:01 +0200

    update

--- FluentBuilder.Generator.Demo/Test.cs
+++ FluentBuilder.Generator.Demo/Test.cs
@@ -1,12 +1,1 @@
-﻿using FluentBuilder;
-
-namespace Demo
-{
-    public class OrderBuilder { } // top‑level type, should not conflict
-
-    public partial class Container
-    {
-        [FluentBuilder]
-        public class Order { }
-    }
-}
+﻿
